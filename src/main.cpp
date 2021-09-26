a45e01dda606352f11ab628be3be1adb6e6afee7
Programier <wladnet28@gmail.com>
2021-09-27 01:55:28 +0300

    Add getKey function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <unistd.h>
+#include <termios.h>
 
-int main(int argc, char *argv[])
+unsigned int getKey(void)
 {
-	std::cout << "Hello world!" << std::endl;
+	struct termios oldattr, newattr;
+	int ch;
+	tcgetattr(STDIN_FILENO, &oldattr);
+	newattr = oldattr;
+	newattr.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+	return ch;
+}
+
+int main()
+{
+	while (true)
+	{
+		std::cout << getKey() << std::endl;
+	}
 }
